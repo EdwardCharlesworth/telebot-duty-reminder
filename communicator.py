1470ac45795589395ade39cda95d18361a57fa5c
Sebastian <v.babtis.klo@freenet.de>
2021-07-18 20:50:04 +0200

    initial commit

--- communicator.py
+++ communicator.py
@@ -52,10 +52,10 @@ def c_new_duty(bot, queue, message):
 
 
 ### for later use
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-def gen_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
-                               InlineKeyboardButton("No", callback_data="cb_no"))
-    return markup
+#from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+#def gen_markup():
+#    markup = InlineKeyboardMarkup()
+#    markup.row_width = 2
+#    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+#                               InlineKeyboardButton("No", callback_data="cb_no"))
+#    return markup
