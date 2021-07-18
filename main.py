fae8eb1582fcfd607a689e3b778b72c7d6a1b0dd
e.charlesworth <icg133@linux.fritz.box>
2021-07-18 21:58:07 +0200

    Basic functionality of reminder.

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def communicator(queue):
     bot.polling()
     print('communicator end')
 
-
 # A thread that reminds chats about events
 def reminder(queue):
+
     data = {
         'chat_id': 1234,
         'type': 'new_duty',
@@ -40,15 +40,80 @@ def reminder(queue):
         'flatmates': ['Ed', 'Clemens', 'Linda', 'Basti'],
         'message': 'Du bist dran',
     }
-    while True:
-        time.sleep(60*10)
-        # Produce some data
-        data = queue.get()
-        date_list = sort_function(date_list, data)
-        if is_today(date_list[0]):
-            data = date_list.pop()
-            bot.send
+    queue.put(data)
 
+    items = []
+
+    WAIT_TIME = 5 # seconds
+    while True:
+
+        # Check if there's anything in the queue
+        if not queue.empty():
+            print("Got new messages")
+
+            #New messages!
+            data = queue.get()
+
+            if data['type']=='new_duty':
+                items.append(chat_reminder_instance(data))
+
+            #date_list = sort_function(date_list, data)
+
+        # Check if you have any items at all
+        if len(items)==0:
+            print("No items found. Waiting 1 second.")
+            time.sleep(1)
+            continue
+        
+        # Check if anything needs to run now
+        for item in items:
+            print("Looking through items")
+            if item.should_print_now():
+                print("Time to print")
+                message = item.print_message_and_cycle()
+                print(message)
+            #else:
+            #    break
+            # This is where you should set something up so
+            # that you don't run through the list constantly.
+            # Sort the list items by when they need to happen.
+            # Then, when they happen, move them to the end of
+            # the item list. So then you only need to check
+            # items until you find one where you don't need to
+            # do anything. Then all the following items don't
+            # need anything either.
+
+        # Wait until it's time to check things again
+        time.sleep(WAIT_TIME) 
+
+class chat_reminder_instance():
+
+    def __init__(self,item):
+        self.chat_id = item['chat_id']
+        self.duty_name = item['name']
+        self.frequency = item['frequency'].days/(24*60*4)
+        self.goal_datetime = item['start_time']
+        self.roster = item['flatmates']
+        self.message = item['message']
+
+    def should_print_now(self):
+        goal = self.goal_datetime
+        now = datetime.datetime.utcnow()
+        if (now-goal).total_seconds() > 0:
+            return True
+        else:
+            return False
+
+    def print_message_and_cycle(self):
+        member = self.roster.pop(0) #Pull the member whose turn it is
+        message = member+": "+self.message#Write the message
+        self.roster.append(member) #Add member whose turn it is to the roster
+        next_datetime = self.goal_datetime + datetime.timedelta(days=self.frequency)
+        self.goal_datetime = next_datetime
+        return message
+
+
+    
 
 
 
